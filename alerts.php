7ee2b45577291196e27526142c833c82c57b9907
Gadi Cohen <dragon@wastelands.net>
2012-11-20 01:11:12 +0200

    alerts API now honors source,fmt.  limit fixes.  more info

--- alerts.php
+++ alerts.php
@@ -4,32 +4,71 @@ include('gadi_db.php');
 
 $limit = isset($_GET['limit']) ? intval($_GET['limit']) : null;
 $lastId = isset($_GET['lastId']) ? intval($_GET['lastId']) : 0;
+$source = isset($_GET['source']) ? explode(",",$_GET['source']) : null;
+$fmt = isset($_GET['fmt']) ? $_GET['fmt'] : 'json';
 
 // BOO :(   Get last 10 alerts
 if ($limit) {
-	$SQL = 'SELECT MIN(alert_id) AS alert_ids
-		FROM alerts GROUP BY time
-		ORDER BY time DESC LIMIT 10,1';
-	$lastId = $dbh->getCol($SQL);
+	$SQL = 'SELECT MAX(alert_id) AS alert_ids
+		FROM alerts';
+		if ($source)
+			$SQL .= ' WHERE source IN ("'.implode('","', $source).'")';
+	$SQL .=	'GROUP BY time
+		ORDER BY alert_id DESC LIMIT '.$limit.',1';
+	$lastId = $dbh->getOne($SQL);
 }
 
 $SQL = 'SELECT alert_id,area_lat,area_long,area_name,
 		UNIX_TIMESTAMP(time) AS time
 	FROM alerts INNER JOIN area USING (area_id)
-	WHERE alert_id > ?
-	ORDER BY time DESC';
-$data1 = $dbh->getAll($SQL, array($lastId));
+	WHERE alert_id > ?';
+if ($source)
+	$SQL .= ' AND source IN ("'.implode('","', $source).'")';
+$SQL .=	' ORDER BY time DESC';
+$data = $dbh->getAll($SQL, array($lastId));
 
-if ($data1) {
-	foreach($data1 as $row1) {
+function correctTypes($data) {
+	foreach($data as $row1) {
 		$row['alert_id'] = intval($row1['alert_id']);
 		$row['area_name'] = $row1['area_name'];
 		$row['time'] = intval($row1['time']);
 		$row['area_lat'] = floatval($row1['area_lat']);
 		$row['area_long'] = floatval($row1['area_long']);
-		$data[] = $row;
+		$typedData[] = $row;
 	}
-	echo json_encode($data);
+	return $typedData;
+}
+
+function htmlpage($inside) {
+	return '
+<html>
+
+<head>
+<title>Tzeva-Adom.Com Feed</title>
+</head>
+
+<body>
+'.$inside.'
+</body>
+</html>';
+}
+
+function htmlize($data) {
+	$out = '';
+	foreach($data as $row) {
+		$out .= $row['time'].' '.$row['area_name'].'<br>'."\n";
+	}
+	return $out;
+}
+
+if ($data) {
+	if ($fmt == "json")
+		echo json_encode(correctTypes($data));
+	elseif ($fmt == "html")
+		echo htmlpage(htmlize($data));	
 } else {
-	echo '[]';
+	if ($fmt == "json")
+		echo '[]';
+	elseif ($fmt == "html")
+		echo htmlpage("<p>No updates</p>");
 }
